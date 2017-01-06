e21f598d2cd601557a0a52d840d521ecccea9216
DaphDC <daphnedc21@gmail.com>
2017-01-05 20:04:47 -0500

    http app

---
+++ src/app/app.component.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit } from '@angular/core';
+import { Response } from '@angular/http';
+
+import { HttpService } from "./http.service";
+
+@Component({
+  moduleId: module.id,
+  selector: 'my-app',
+  templateUrl: 'app.component.html'
+})
+export class AppComponent implements OnInit {
+  items: any[] = [];
+  asyncString = this.httpService.getData();
+
+  constructor(private httpService: HttpService) {
+
+  }
+
+  ngOnInit(){
+    this.httpService.getData()
+      .subscribe(
+        (data: string) => console.log(data)
+    );
+  }
+
+  onSubmit(username: string, email: string) {
+    this.httpService.sendData({'username': username, 'email': email})
+      .subscribe(
+        data => console.log(data),
+        error => console.log(error)
+      );
+  }
+
+  onGetData() {
+    this.httpService.getOwnData()
+      .subscribe(
+        data => {
+          const myArray = [];
+          for (let key in data) {
+            myArray.push(data[key]);
+          }
+          this.items = myArray;
+        }
+      );
+  }
+  
+}
